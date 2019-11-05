88361bee5b90a33b3c2dcd06c655ade430fe839f
Teo Samaržija <46264381+FlatAssembler@users.noreply.github.com>
2019-11-05 16:35:46 +0100

    Add files via upload
    Added qsort.cpp, to compare the number of comparisons each algorithm do, and to more fairly compare their overall speeds.

---
+++ QuickSort/qsort.cpp
@@ -0,0 +1,38 @@
+#include <cmath>
+#include <cstdio>
+#include <algorithm>
+
+int niz[32768];
+int brojac=0;
+bool usporedba(int a, int b) {
+    brojac++;
+    return a<b;
+}
+
+int main() {
+    using std::printf;
+    using std::sort;
+    using std::scanf;
+    int n;
+#ifdef IspisujPoruke
+	printf("Unesite koliko cete brojeva unijeti.\n");
+#endif
+	scanf("%d",&n);
+#ifdef IspisujPoruke
+	printf("Unesite te brojeve:\n");
+#endif
+	for (int i=0; i<n; i++)
+		scanf("%d",&niz[i]);
+	sort(niz,niz+n,usporedba);
+#ifdef IspisujPoruke
+	printf("Sortirani niz je:\n");
+#endif
+	for (int i=0; i<n; i++) printf("%d\n",niz[i]);
+#ifdef IspisujPoruke
+	printf("Unutrasnja petlja izvrsila se %d puta.\n",brojac);
+	printf("Ocekivani broj ponavljanja te petlje, po formuli n*log2(n), bio bi %.1f.\n",n*log(n)/log(2));
+	printf("Pritisnite CTRL+C za izlaz.\n");
+	while (1);
+#endif
+	return 0;
+}
