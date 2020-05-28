59f963efda3e896c0c30342c0cb049b343c0a7a2
Teo Samaržija <46264381+FlatAssembler@users.noreply.github.com>
2020-05-28 13:24:52 +0200

    Fixed a bug preventing qsort.aec from compiling.

--- control.js
+++ control.js
@@ -4,12 +4,14 @@ isAssembly = false;
 function compileString(str) {
   str = str.replace(/^\s*/, "");
   if (/^AsmEnd/.test(str)) {
+    asm(";" + str);
     asm(";Inline assembly ended.");
     isAssembly = false;
     return;
   }
   if (/^AsmStart/.test(str)) {
     isAssembly = true;
+    asm(";" + str);
     asm(";Inline assembly begins.");
     return;
   }
@@ -19,17 +21,25 @@ function compileString(str) {
   }
   asm(";" + str);
   if (!str || str[0] == ";") return;
+  var isString = false;
+  for (var i = 0; i < str.length; i++)
+    if (str[i] == '"' || str[i] == "'") isString = !isString;
+    else if (str[i] == ";") {
+      //Obrisi komentar, ako se slucajno nesto kao '<=' ili ':=' nalazi u komentaru, da ne smeta parseru.
+      str = str.substr(0, i - 1);
+      break;
+    }
   asm("finit");
   if (str.indexOf("<=") + 1) {
     var constantName = str.substr(0, str.indexOf("<="));
-    constantName = constantName.replace(/\s*$/, "");
+    constantName = constantName.replace(/\s*$/g, "");
     var constantValue = str.substr(str.indexOf("<=") + "<=".length);
     asm("jmp " + constantName + "$");
     asm(constantName + " db " + constantValue);
     asm(constantName + "$:");
   } else if (str.indexOf(":=") + 1) {
     var variableName = str.substr(0, str.indexOf(":="));
-    variableName = variableName.replace(/\s*$/, "");
+    variableName = variableName.replace(/\s*$/g, "");
     var arth = str.substr(str.indexOf(":=") + ":=".length);
     parseArth(tokenizeArth(arth)).compile();
     asm("fstp dword [result]");
@@ -38,23 +48,24 @@ function compileString(str) {
       var indexOfBracket =
         (variableName.indexOf("[") + 1 || variableName.indexOf("(") + 1) - 1;
       var arrayName = variableName.substr(0, indexOfBracket);
+      arrayName.replace(/\s*$/g, "");
       var subscript = variableName.substr(
         indexOfBracket + 1,
         variableName.length - indexOfBracket - 2
       );
       parseArth(tokenizeArth(subscript)).compile();
       asm("fistp dword [result]");
-      asm("mov ebx,[result]");
+      asm("mov ebx, dword [result]");
       asm("mov dword [" + arrayName + "+4*ebx],edx");
     } else asm("mov dword [" + variableName + "],edx");
   } else if (/^If\s/.test(str)) {
     var arth = str.substr("If ".length);
     parseArth(tokenizeArth(arth)).compile();
     asm("fistp dword [result]");
-    asm("mov eax,[result]");
+    asm("mov eax, dword [result]");
     asm("test eax,eax");
-    var label1 = "l" + Math.floor(Math.random() * 1000000);
-    var label2 = "l" + Math.floor(Math.random() * 1000000);
+    var label1 = "ElseLabel" + Math.floor(Math.random() * 1000000);
+    var label2 = "EndIfLabel" + Math.floor(Math.random() * 1000000);
     stack.push(label2);
     stack.push(label1);
     asm("jz " + label1);
@@ -73,9 +84,9 @@ function compileString(str) {
     var arth = str.substr("ElseIf ".length);
     parseArth(tokenizeArth(arth)).compile();
     asm("fistp dword [result]");
-    asm("mov eax,[result]");
+    asm("mov eax, dword [result]");
     asm("test eax,eax");
-    var newLabelOfElse = "l" + Math.floor(Math.random() * 1000000);
+    var newLabelOfElse = "ElseLabel" + Math.floor(Math.random() * 1000000);
     stack.push(labelOfEndIf);
     stack.push(newLabelOfElse);
     asm("jz " + newLabelOfElse);
@@ -89,13 +100,13 @@ function compileString(str) {
     hasElse.push(true);
   } else if (/^While\s/.test(str)) {
     var arth = str.substr("While ".length);
-    var label1 = "l" + Math.floor(Math.random() * 1000000);
+    var label1 = "WhileLabel" + Math.floor(Math.random() * 1000000);
     stack.push(label1);
     asm(label1 + ":");
     parseArth(tokenizeArth(arth)).compile();
-    var label2 = "l" + Math.floor(Math.random() * 1000000);
+    var label2 = "EndWhileLabel" + Math.floor(Math.random() * 1000000);
     asm("fistp dword [result]");
-    asm("mov eax,[result]");
+    asm("mov eax, dword [result]");
     asm("test eax,eax");
     asm("je " + label2);
     stack.push(label2);
