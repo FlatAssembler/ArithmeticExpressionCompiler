5e92c21a72bc137229716db9fda220aed3b7f646
Teo Samaržija <46264381+FlatAssembler@users.noreply.github.com>
2020-07-05 14:42:21 +0200

    Fixed a bug in "random_tester".

--- recursive_HybridSort/randomly_generate_numbers.cpp
+++ recursive_HybridSort/randomly_generate_numbers.cpp
@@ -6,12 +6,12 @@
 
 extern "C" { // Za GNU Linker (koji se dobije uz Linux i koristi ga GCC), AEC
              // jezik je dijalekt C-a, a moj compiler je C compiler.
-float result, originalni_niz[32768], kopija_originalnog_niza[32768],
-    pomocni_niz[32768], i, gdje_smo_u_prvom_nizu, gdje_smo_u_drugom_nizu,
-    gornja_granica, donja_granica, sredina_niza, stog_sa_sredinama_niza[1024],
-    stog_s_donjim_granicama[1024], stog_s_gornjim_granicama[1024], vrh_stoga,
-    pomocna_varijabla_za_zamijenu, gdje_je_pivot, j, pivot,
-    koliko_usporedbi_ocekujemo_od_QuickSorta,
+float result, originalni_niz[1 << 16], kopija_originalnog_niza[1 << 16],
+    pomocni_niz[1 << 16], i, gdje_smo_u_prvom_nizu, gdje_smo_u_drugom_nizu,
+    gornja_granica, donja_granica, sredina_niza,
+    stog_sa_sredinama_niza[1 << 10], stog_s_donjim_granicama[1 << 10],
+    stog_s_gornjim_granicama[1 << 10], vrh_stoga, pomocna_varijabla_za_zamijenu,
+    gdje_je_pivot, j, pivot, koliko_usporedbi_ocekujemo_od_QuickSorta,
     koliko_usporedbi_ocekujemo_od_MergeSorta, razvrstanost,
     Eulerov_broj_na_koju_potenciju, polinom_pod_apsolutnom,
     razvrstanost_na_potenciju[8],
@@ -21,10 +21,10 @@ float result, originalni_niz[32768], kopija_originalnog_niza[32768],
         0; // GNU linker omogucuje da se varijable ne deklariraju ne samo u
            // razlicitim datotekama, nego i u razlicitim jezicima.
 void hybrid_sort(); //".global hybrid_sort" iz "hybrid_sort.aec". U C++-u ga
-                    //morate deklarirati da biste ga mogli koristiti. C++ nije
-                    //kao JavaScript ili AEC u tom pogledu, C++ pokusava pronaci
-                    //krivo natipkana imena varijabli i funkcija vec za vrijeme
-                    //compiliranja.
+                    // morate deklarirati da biste ga mogli koristiti. C++ nije
+                    // kao JavaScript ili AEC u tom pogledu, C++ pokusava
+                    // pronaci krivo natipkana imena varijabli i funkcija vec za
+                    // vrijeme compiliranja.
 }
 
 int main() {
@@ -32,16 +32,23 @@ int main() {
             << std::endl;
   int n;
   std::cin >> n;
+  auto distribution = std::uniform_real_distribution<float>(-n, n);
+  auto generator =
+      std::default_random_engine(); // Moramo paziti da se generator
+                                    // pseudo-nasumicnih brojeva inicijalizira
+                                    // samo jednom, inace ce na Linuxu
+                                    // generirati obrnuto sortirani niz (a ne
+                                    // nasumicno poredan). To su nam spomenuli
+                                    // na fakultetu, no ja to nisam uzeo
+                                    // ozbiljno, pa sam se sada opekao.
   std::cout << "Generiram niz od " << n << " nasumicnih decimalnih brojeva..."
             << std::endl;
   std::generate_n(
       &originalni_niz[0], n,
-      [=]() -> float { // C++-ove lambda funkcije, GCC ih podrzava od 2007, a
+      [&]() -> float { // C++-ove lambda funkcije, GCC ih podrzava od 2007, a
                        // komercijalni compileri jos od ranije. Nadajmo se da
                        // netko nece pokusati ukucati ovaj program u arhajski
                        // C++ compiler.
-        auto distribution = std::uniform_real_distribution<float>(-n, n);
-        auto generator = std::default_random_engine();
         return distribution(generator);
       });
   std::copy_n(&originalni_niz[0], n, &kopija_originalnog_niza[0]);
